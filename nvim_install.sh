6d605981c6e42d756b8eeaeb378bfdf01f057f33
Omar <omarnelson1533@gmail.com>
2021-03-26 20:59:24 -0600

    Created install script for linux

--- nvim_install.sh
+++ nvim_install.sh
@@ -1,4 +1,18 @@
-TARGET=$(dirname ${0})
-LINK=~/.config/nvim/
+#!/usr/bin/env bash
 
-ln -s ${TARGET} ${LINK}
+# Make sure the config directory exists
+mkdir -p ~/.config/nvim
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/init.vim
+LINK=~/.config/nvim/init.vim
+
+echo "Creating a link to ${SCRIPTPATH} with name ${LINK}"
+
+ln -s ${SCRIPTPATH} ${LINK}
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+LINK=~/.vim
+
+echo "Creating a link to ${SCRIPTPATH} with name ${LINK}"
+
+ln -s ${SCRIPTPATH} ${LINK}
